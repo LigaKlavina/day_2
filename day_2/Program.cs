dc6f4c7119898760ba0c53f02afa7173afa80bf0
LigaKlavina <ligaiklavinai@gmail.com>
2018-05-07 20:01:23 +0300

    day 2
    Pirmais commits 2. dienai/projekta izveide

---
+++ day_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String dati;
+            Console.WriteLine("Ludzu ievadiet dzimsanas datus!");
+            dati = Console.ReadLine();
+            Console.WriteLine("Jus esat dzimis " + dati);
+            Console.ReadLine();
+
+        }
+    }
+}
